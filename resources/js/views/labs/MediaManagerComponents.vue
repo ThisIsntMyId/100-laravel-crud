4ab54070b4bd4993fc6a1ed9204e72445fdcc105
Dishant Pal <dishantpal37@gmail.com>
2020-01-06 18:00:23 +0530

    Made File Manager Form Component

--- resources/js/views/labs/MediaManagerComponents.vue
+++ resources/js/views/labs/MediaManagerComponents.vue
@@ -2,27 +2,27 @@
   <div class="app-container">
     <h1>New</h1>
     <div>
-      <FileManagerInput showImg />
+      <FileManagerInput :field-value.sync="value1" showImg />
     </div>
     <br />
     <br />
     <div>
-      <FileManagerInput />
+      <FileManagerInput :field-value.sync="value2" />
     </div>
     <br />
     <br />
     <div>
-      <FileManagerInput showImg />
+      <FileManagerInput :field-value.sync="value3" showImg />
     </div>
     <br />
     <br />
     <div>
-      <FileManagerInput recursive />
+      <FileManagerInput :field-value.sync="value4" recursive />
     </div>
     <br />
     <br />
     <div>
-      <FileManagerInput recursive showImg />
+      <FileManagerInput :field-value.sync="value5" recursive showImg />
     </div>
     <br />
   </div>
@@ -35,7 +35,18 @@ export default {
   components: { FileManagerInput },
   updated() {},
   data() {
-    return {};
+    return {
+      value1: '[]',
+      // value1: "[\"/storage/download (2).jpg\"]",
+      value2: '[]',
+      // value2: "[\"/storage/megasiteDev_LV.sql.zip\",\"/storage/app.js\",\"/storage/composer.json\"]",
+      value3: '[]',
+      // value3: "[\"/storage/My Images 2/cat images/download (2).jpg\",\"/storage/My Images 2/cat images/download (3).jpg\",\"/storage/My Images 2/cat images/download (4).jpg\",\"/storage/My Images 2/cat images/images (1).jpg\"]",
+      // value4: '[]',
+      value4: "[\"/storage/app.js\",\"/storage/tag/icon/uOkDN1HAco9rqrKRnisF6bkOHQwcDFY4GEhRWfzq.png\"]",
+      // value5: '[]',
+      value5: "[\"/storage/screen shots/Screenshot_5.png\",\"/storage/screen shots/Screenshot_2.png\",\"/storage/other images/37392486-dbz-wallpapers.jpg\",\"/storage/other images/95327.jpg\",\"/storage/My Images 2/cat images/download (2).jpg\",\"/storage/My Images 2/cat images/download (4).jpg\"]",
+    };
   },
   methods: {},
 };
